0765e47bc88bf8ed83e3410641e27dfbfd9899d7
Gabriel Filip <37806829+gabf48@users.noreply.github.com>
2021-05-23 22:40:29 +0300

    firebase - save and fetch data

---
+++ src/app/shared/data-storage.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+
+import { Recipe } from '../recipes/recipe.model';
+import { RecipeService } from '../recipes/recipe.service';
+
+@Injectable({providedIn: 'root'})
+export class DataStorageService {
+    constructor(private http: HttpClient, private recipeService: RecipeService) {}
+
+    storeRecipes() {
+        const recipes = this.recipeService.getRecipes();
+       return this.http.put('https://udemy-project-c939b-default-rtdb.firebaseio.com/recipes.json', recipes).subscribe(response => {
+           console.log(response);
+       });
+    }
+
+    fetchRecipes(){
+        this.http
+        .get<Recipe[]>('https://udemy-project-c939b-default-rtdb.firebaseio.com/recipes.json')
+        .subscribe(recipes => {
+            this.recipeService.setRecipes(recipes);
+        })
+    }
+ }
