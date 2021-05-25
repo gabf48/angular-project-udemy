855b069b4deefa5040f415666d85d76810241020
Gabriel Filip <37806829+gabf48@users.noreply.github.com>
2021-05-25 23:01:50 +0300

    Attaching the Token with an Interceptor
    logged user can save in db and get from there any recipe

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -27,16 +27,11 @@ export class DataStorageService {
   }
 
   fetchRecipes() {
-   return this.authService.user.pipe(
-     take(1), 
-     exhaustMap(user => {
+  
       return this.http.get<Recipe[]>(
-        'https://udemy-project-c939b-default-rtdb.firebaseio.com/recipes.json',
-        {
-          params: new HttpParams().set('auth', user.token)
-        }
-      );
-    }),
+        'https://udemy-project-c939b-default-rtdb.firebaseio.com/recipes.json'
+      )
+      .pipe(
         map(recipes => {
           return recipes.map(recipe => {
             return {
